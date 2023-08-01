6fcea870473b78118c4928cd8c8abcf2868266d8
nathalieassis <nathaliesenac8@gmail.com>
2023-08-01 09:41:59 -0300

    calculadora

---
+++ index.js
@@ -0,0 +1,28 @@
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+    charKeyBtn.addEventListener('click', function () {
+        const value = charKeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function () {
+    input.value = ""
+    input.focus()
+})
+
+const calculate = () => {
+    resultInput.value = "ERROR"
+    resultInput.classList.add("error")
+    const result = eval(input.value)
+    resultInput.value = result
+    resultInput.classList.remove("error")
+}
+
+
+document.getElementById("igual").addEventListener('click', calculate)
